c3489c6bd78c3dd53999fea24f68e331e5d3ed6c
Krzysztof Kania <01149678@pw.edu.pl>
2022-12-27 19:52:21 +0100

    Added mouse control

--- base_window.py
+++ base_window.py
@@ -1,9 +1,10 @@
+import math
 from pathlib import Path
 import imageio
 import numpy as np
 from moderngl import DEPTH_TEST, TRIANGLE_STRIP
 from moderngl_window import WindowConfig
-from pyrr import Matrix44
+from pyrr import Matrix44, Vector4
 
 from shader_utils import get_shaders
 
@@ -24,6 +25,8 @@ class MainWindowConfig(WindowConfig):
         self.load_png_heightmap(self.argv.map_name)
         self.generate_terrain()
         self.init_shaders_variables()
+
+        self.lookat = (-self.x_range * 3 / 4, -self.y_range * 3 / 4, (self.x_range + self.y_range) / 3)
 
     def load_png_heightmap(self, map_name):
         raw_heightmap = imageio.v3.imread(f"./resources/heightmaps/{map_name}.png")
@@ -106,15 +109,45 @@ class MainWindowConfig(WindowConfig):
         self.input_color.value = (np.sin(time) / 2 + 0.5, 1.0, 0.0)
 
         proj = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 2000.0)
-        lookat = Matrix44.look_at(  # should be appropriate for most of the heightmaps
-            (-self.x_range * 3 / 4, -self.y_range * 3 / 4, (self.x_range + self.y_range) / 3),
-            (0.0, 0.0, 1.0),
-            (0.0, 0.0, 1.0),
-        )
 
-        self.tr_matrix.write((proj * lookat
-                              * Matrix44.from_z_rotation(time / 10 * 2 * np.pi)
+        self.tr_matrix.write((proj * Matrix44.look_at(self.lookat, (0.0, 0.0, 1.0),
+                                                      (0.0, 0.0, 1.0), )
+                              * Matrix44.from_z_rotation(1 / 10 * 2 * np.pi)
                               * Matrix44.from_translation((-self.x_range / 2, -self.x_range / 2, 1.0))
                               * Matrix44.from_scale((1.0, 1.0, self.height_scale))
                               ).astype('float32'))
         self.vao.render(TRIANGLE_STRIP)
+
+    def mouse_drag_event(self, x, y, dx, dy):
+        # rotate camera around the robot
+        width, height = self.wnd.size
+        if width > height * self.aspect_ratio:
+            width = height * self.aspect_ratio
+        else:
+            height = width / self.aspect_ratio
+        scaled_dx = dx * 2.0 / width
+        scaled_dy = dy * 2.0 / height
+        radius = math.sqrt(self.lookat[0] ** 2 + self.lookat[1] ** 2 + self.lookat[2] ** 2)
+        eye_vec4 = Vector4(self.lookat + (1.0,))
+        current_angle_horizontal = math.atan2(self.lookat[1], self.lookat[0])
+        new_eye = Matrix44.from_eulers((
+            -4 * math.sin(current_angle_horizontal) * scaled_dy,
+            4 * scaled_dx,
+            4 * math.cos(current_angle_horizontal) * scaled_dy
+        )) * eye_vec4
+        # if camera is near the zenith or nadir, try to limit its "jumping" behavior
+        new_radius_horizontal = math.sqrt(new_eye[0] ** 2 + new_eye[1] ** 2)
+        if radius > 0.0 and new_radius_horizontal / radius > 0.1:
+            self.lookat = tuple(new_eye)[:3]
+            
+    def mouse_scroll_event(self, x_offset, y_offset):
+        # make the object appear bigger when scrolling up and smaller when scrolling down
+        y_offset /= 2
+        radius = math.sqrt(self.lookat[0] ** 2 + self.lookat[1] ** 2 + self.lookat[2] ** 2)
+        radius *= (1 - y_offset)
+        if radius >= 1.0:  # prevent unwanted scene rotation
+            self.lookat = (
+                self.lookat[0] * (1 - y_offset),
+                self.lookat[1] * (1 - y_offset),
+                self.lookat[2] * (1 - y_offset)
+            )
