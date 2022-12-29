3859722f334f916337cac1624f67f4b5d8d60063
Krzysztof Kania <01149678@pw.edu.pl>
2022-12-29 12:48:03 +0100

    Added types

--- base_window.py
+++ base_window.py
@@ -1,52 +1,57 @@
 import math
+from argparse import ArgumentParser
 from pathlib import Path
+
 import imageio
 import numpy as np
-from moderngl import DEPTH_TEST, TRIANGLE_STRIP
+from _moderngl import Uniform
+from moderngl import DEPTH_TEST, TRIANGLE_STRIP, Program, Buffer, VertexArray
 from moderngl_window import WindowConfig
+from numpy import ndarray, uint8
 from pyrr import Matrix44, Vector4
 
-from shader_utils import get_shaders
+from shader_utils import get_shaders, ShaderCollection
 
 
 class MainWindowConfig(WindowConfig):
-    gl_version = (3, 3)
-    title = 'Environment Rendering'
-    resource_dir = (Path(__file__).parent / 'resources').resolve()
+    gl_version: tuple[int, int] = (3, 3)
+    title: str = 'Environment Rendering'
+    resource_dir: Path = (Path(__file__).parent / 'resources').resolve()
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: dict[str, dict]) -> None:
         super(MainWindowConfig, self).__init__(**kwargs)
 
-        shaders = get_shaders()
-        self.program = self.ctx.program(vertex_shader=shaders[self.argv.shader_name].vertex_shader,
-                                        fragment_shader=shaders[self.argv.shader_name].fragment_shader)
+        shaders: dict[str, ShaderCollection] = get_shaders()
+        self.program: Program = self.ctx.program(vertex_shader=shaders[self.argv.shader_name].vertex_shader,
+                                                 fragment_shader=shaders[self.argv.shader_name].fragment_shader)
 
-        self.height_scale = self.argv.height_scale if self.argv.height_scale is not None else 1.0
+        self.height_scale: float = self.argv.height_scale if self.argv.height_scale is not None else 1.0
 
         self.load_png_heightmap(self.argv.map_name)
         self.generate_terrain()
         self.init_shaders_variables()
 
-        self.lookat = (-self.x_range * 3 / 4, -self.y_range * 3 / 4, (self.x_range + self.y_range) / 3)
+        self.lookat: tuple[float, float, float] = (
+            -self.x_range * 3 / 4, -self.y_range * 3 / 4, (self.x_range + self.y_range) / 3)
 
-    def load_png_heightmap(self, map_name):
-        raw_heightmap = imageio.v3.imread(f"./resources/heightmaps/{map_name}.png")
-        self.x_range = raw_heightmap.shape[0]
-        self.y_range = raw_heightmap.shape[1]
+    def load_png_heightmap(self, map_name: str) -> None:
+        raw_heightmap: ndarray = imageio.v3.imread(f"./resources/heightmaps/{map_name}.png")
+        self.x_range: int = raw_heightmap.shape[0]
+        self.y_range: int = raw_heightmap.shape[1]
 
-        self.height_map = np.empty([self.x_range, self.y_range, 3])
+        self.height_map: ndarray = np.empty([self.x_range, self.y_range, 3])
         for x_i in range(self.x_range):
             for y_i in range(self.y_range):
                 # this is necessary because png files can store colours as rgb or grayscale values, which creates issues
-                z_val = raw_heightmap[x_i][y_i] if len(raw_heightmap.shape) == 2 else raw_heightmap[x_i][y_i][0]
+                z_val: uint8 = raw_heightmap[x_i][y_i] if len(raw_heightmap.shape) == 2 else raw_heightmap[x_i][y_i][0]
                 self.height_map[x_i][y_i] = np.array([x_i, y_i, z_val])  # add ' / 3' to z_val if stuff's too darn high
 
-    def generate_terrain(self):
-        vertices = np.empty([self.x_range * self.y_range, 3])
-        vertices_and_normals = np.empty([self.x_range * self.y_range, 6])
-        indices = np.empty([3 * 2 * (self.x_range - 1) * (self.y_range - 1) + self.y_range - 1])
+    def generate_terrain(self) -> None:
+        vertices: ndarray = np.empty([self.x_range * self.y_range, 3])
+        vertices_and_normals: ndarray = np.empty([self.x_range * self.y_range, 6])
+        indices: ndarray = np.empty([3 * 2 * (self.x_range - 1) * (self.y_range - 1) + self.y_range - 1])
 
-        def append_to_indices(vertex):
+        def append_to_indices(vertex: int) -> None:
             indices[append_to_indices.counter] = vertex
             append_to_indices.counter += 1
 
@@ -54,28 +59,28 @@ class MainWindowConfig(WindowConfig):
 
         for x_i in range(self.x_range):
             for y_i in range(self.y_range):
-                v_idx = y_i * self.x_range + x_i
+                v_idx: int = y_i * self.x_range + x_i
                 vertices[v_idx] = self.height_map[x_i][y_i]
                 vertices_and_normals[v_idx] = [*self.height_map[x_i][y_i], 0, 0, 0]
 
-        def create_triangle(vertex_1, vertex_2, vertex_3):
-            append_to_indices(vertex_1)
-            append_to_indices(vertex_2)
-            append_to_indices(vertex_3)
-            edge_1 = vertices[vertex_2] - vertices[vertex_1]
-            edge_2 = vertices[vertex_3] - vertices[vertex_1]
-            cross = np.cross(edge_1, edge_2)
+        def create_triangle(v1: int, v2: int, v3: int) -> None:
+            append_to_indices(v1)
+            append_to_indices(v2)
+            append_to_indices(v3)
+            edge_1: ndarray = vertices[v2] - vertices[v1]
+            edge_2: ndarray = vertices[v3] - vertices[v1]
+            cross: ndarray = np.cross(edge_1, edge_2)
 
-            for vertex in [vertex_1, vertex_2, vertex_3]:
+            for vertex in [v1, v2, v3]:
                 for i in range(3):
                     vertices_and_normals[vertex][3 + i] += cross[i]
 
         for x_i in range(self.x_range - 1):
             for y_i in range(self.y_range - 1):
                 # create two triangles forming a quadrangle (sorta, if you ignore the z-values)
-                vertex_1 = x_i + y_i * self.x_range
-                vertex_2 = x_i + 1 + y_i * self.x_range
-                vertex_3 = x_i + (y_i + 1) * self.x_range
+                vertex_1: int = x_i + y_i * self.x_range
+                vertex_2: int = x_i + 1 + y_i * self.x_range
+                vertex_3: int = x_i + (y_i + 1) * self.x_range
                 create_triangle(vertex_1, vertex_2, vertex_3)
 
                 vertex_1 = x_i + 1 + y_i * self.x_range
@@ -85,21 +90,21 @@ class MainWindowConfig(WindowConfig):
 
             append_to_indices(-1)  # This is here to get rid of unwanted artifacts
 
-        vbo = self.ctx.buffer(np.array(vertices_and_normals).astype('float32').tobytes())
-        ibo = self.ctx.buffer(np.array(indices).astype('int32').tobytes())
-        self.vao = self.ctx.vertex_array(
+        vbo: Buffer = self.ctx.buffer(np.array(vertices_and_normals).astype('float32').tobytes())
+        ibo: Buffer = self.ctx.buffer(np.array(indices).astype('int32').tobytes())
+        self.vao: VertexArray = self.ctx.vertex_array(
             self.program,
             [(vbo, '3f 3f', 'in_position', 'in_normal')],
             index_buffer=ibo,
             index_element_size=4,
         )
 
-    def init_shaders_variables(self):
-        self.tr_matrix = self.program['tr_matrix']
-        self.input_color = self.program['obj_color']
+    def init_shaders_variables(self) -> None:
+        self.tr_matrix: Uniform = self.program['tr_matrix']
+        self.input_color: Uniform = self.program['obj_color']
 
     @classmethod
-    def add_arguments(cls, parser):
+    def add_arguments(cls, parser: ArgumentParser) -> None:
         parser.add_argument('--shader_name', type=str, required=True,
                             help='Name of the shader to look for in the shader_path directory')
         parser.add_argument('--map_name', type=str, required=True, help='Name of the map to load')
@@ -107,12 +112,12 @@ class MainWindowConfig(WindowConfig):
                                                                                'that enables the user to scale the '
                                                                                'height of the map (defaults to 1.0)')
 
-    def render(self, time: float, frame_time: float):
+    def render(self, time: float, frame_time: float) -> None:
         self.ctx.clear(1.0, 1.0, 1.0, 0.0)
         self.ctx.enable(DEPTH_TEST)
         self.input_color.value = (np.sin(time) / 2 + 0.5, 1.0, 0.0)
 
-        proj = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 2000.0)
+        proj: Matrix44 = Matrix44.perspective_projection(45.0, self.aspect_ratio, 0.1, 2000.0)
 
         self.tr_matrix.write((proj * Matrix44.look_at(self.lookat, (0.0, 0.0, 1.0),
                                                       (0.0, 0.0, 1.0), )
@@ -122,32 +127,32 @@ class MainWindowConfig(WindowConfig):
                               ).astype('float32'))
         self.vao.render(TRIANGLE_STRIP)
 
-    def mouse_drag_event(self, x, y, dx, dy):
+    def mouse_drag_event(self, x: int, y: int, dx: int, dy: int) -> None:
         # rotate camera around the robot
         width, height = self.wnd.size
         if width > height * self.aspect_ratio:
             width = height * self.aspect_ratio
         else:
             height = width / self.aspect_ratio
-        scaled_dx = dx * 2.0 / width
-        scaled_dy = dy * 2.0 / height
-        radius = math.sqrt(self.lookat[0] ** 2 + self.lookat[1] ** 2 + self.lookat[2] ** 2)
-        eye_vec4 = Vector4(self.lookat + (1.0,))
-        current_angle_horizontal = math.atan2(self.lookat[1], self.lookat[0])
-        new_eye = Matrix44.from_eulers((
+        scaled_dx: float = dx * 2.0 / width
+        scaled_dy: float = dy * 2.0 / height
+        radius: float = math.sqrt(self.lookat[0] ** 2 + self.lookat[1] ** 2 + self.lookat[2] ** 2)
+        eye_vec4: Vector4 = Vector4(self.lookat + (1.0,))
+        current_angle_horizontal: float = math.atan2(self.lookat[1], self.lookat[0])
+        new_eye: Vector4 = Matrix44.from_eulers((
             -4 * math.sin(current_angle_horizontal) * scaled_dy,
             4 * scaled_dx,
             4 * math.cos(current_angle_horizontal) * scaled_dy
         )) * eye_vec4
         # if camera is near the zenith or nadir, try to limit its "jumping" behavior
-        new_radius_horizontal = math.sqrt(new_eye[0] ** 2 + new_eye[1] ** 2)
+        new_radius_horizontal: float = math.sqrt(new_eye[0] ** 2 + new_eye[1] ** 2)
         if radius > 0.0 and new_radius_horizontal / radius > 0.1:
             self.lookat = tuple(new_eye)[:3]
 
-    def mouse_scroll_event(self, x_offset, y_offset):
+    def mouse_scroll_event(self, x_offset: float, y_offset: float) -> None:
         # make the object appear bigger when scrolling up and smaller when scrolling down
         y_offset /= 2
-        radius = math.sqrt(self.lookat[0] ** 2 + self.lookat[1] ** 2 + self.lookat[2] ** 2)
+        radius: float = math.sqrt(self.lookat[0] ** 2 + self.lookat[1] ** 2 + self.lookat[2] ** 2)
         radius *= (1 - y_offset)
         if radius >= 1.0:  # prevent unwanted scene rotation
             self.lookat = (
--- shader_utils.py
+++ shader_utils.py
@@ -1,29 +1,31 @@
 import os
 from dataclasses import dataclass
+from typing import List, Dict, Any
+
 
 @dataclass
 class ShaderCollection:
     fragment_shader: str = None
     vertex_shader: str = None
 
-    def assign_shader(self, extension, shader_text):
+    def assign_shader(self, extension: str, shader_text: str) -> None:
         if extension in ['.frag']:
             self.fragment_shader = shader_text
 
         if extension in ['.vert']:
             self.vertex_shader = shader_text
 
-    def is_valid_collection(self):
+    def is_valid_collection(self) -> str:
         return self.vertex_shader and self.fragment_shader
 
 
-def _gather_shader_files(shader_directory_path: str) -> dict:
-    file_names = os.listdir(shader_directory_path)
+def _gather_shader_files(shader_directory_path: str) -> dict[str, list[str]]:
+    file_names: list[str] = os.listdir(shader_directory_path)
 
-    shaders = {}
+    shaders: dict[str, list[str]] = {}
 
     for file_name in file_names:
-        basename = os.path.splitext(file_name)[0]
+        basename: str = os.path.splitext(file_name)[0]
         shaders.setdefault(basename, [])
         shaders[basename].append(os.path.join(shader_directory_path, file_name))
 
@@ -32,21 +34,21 @@ def _gather_shader_files(shader_directory_path: str) -> dict:
 
 def _load_shader(shader_path: str) -> str:
     with open(shader_path) as f:
-        shader_text = f.read()
+        shader_text: str = f.read()
 
     return shader_text
 
 
 def get_shaders() -> dict[str, ShaderCollection]:
-    shaders = {}
-    gathered_files = _gather_shader_files("./resources/shaders/")
+    shaders: dict[str, ShaderCollection] = {}
+    gathered_files: dict[str, list[str]] = _gather_shader_files("./resources/shaders/")
 
     for identifier, shader_path_list in gathered_files.items():
-        shader_collection = ShaderCollection()
+        shader_collection: ShaderCollection = ShaderCollection()
 
         for shader_path in shader_path_list:
-            extension = os.path.splitext(shader_path)[1]
-            shader_text = _load_shader(shader_path)
+            extension: str = os.path.splitext(shader_path)[1]
+            shader_text: str = _load_shader(shader_path)
             shader_collection.assign_shader(extension, shader_text)
 
         if not shader_collection.is_valid_collection():
